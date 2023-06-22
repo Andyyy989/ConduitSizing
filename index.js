5f1efe1fbe4aa85c5323e815d44457c9c296a25b
Andy Li <andylhy99@gmail.com>
2023-06-22 09:34:43 -0400

    created for approval

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const btnEl = document.getElementById("btn");
 
-const amount_1 = document.getElementById("inputBox2");
-const wire_size_1 = document.getElementById("inputBox3");
-const amount_2 = document.getElementById("inputBox4");
-const wire_size_2 = document.getElementById("inputBox5");
+const RawAmount1 = document.getElementById("Amount1");
+const RawWireSize1 = document.getElementById("WireSize1");
+const RawAmount2 = document.getElementById("Amount2");
+const RawWireSize2 = document.getElementById("WireSize2");
 const result = document.getElementById("result");
+const RawType = document.getElementById("type");
+
+// const typeToDict {
+
+// };
 
 function add() {
-    const amount1 = amount_1.value;
-    const wireSize1 = wire_size_1.value;
-    const amount2 = amount_2.value;
-    const wireSize2 = wire_size_2.value;
+    const amount1 = RawAmount1.value;
+    const wireSize1 = RawWireSize1.value;
+    const amount2 = RawAmount2.value;
+    const wireSize2 = RawWireSize2.value;
+    const type = RawType.value;
 
-    const result_1 = parseInt(amount1) + parseInt(wireSize1) + parseInt(amount2) + parseInt(wireSize2);
-    result.innerText = `Your size is ${result_1}`;
+    // const result_1 = parseInt(amount1) + parseInt(wireSize1) + parseInt(amount2) + parseInt(wireSize2);
+    // result.innerText = `Your size is ${result_1}`;
+    result.innerText = `Your type is ${type}`;
 }
 
 btnEl.addEventListener("click", add);
